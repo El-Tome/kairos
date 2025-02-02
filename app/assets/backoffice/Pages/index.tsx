21b005d4634b46279120d795d44a2361c51cd64f
grintzdel <mathis.oudin@etu.uca.fr>
2025-02-02 14:53:07 +0100

    :necktie: Added Pages export files

---
+++ app/assets/backoffice/Pages/index.tsx
@@ -0,0 +1,14 @@
+/**
+ * Backoffice Pages Index
+ * 
+ * This file exports all admin pages components used by Inertia:
+ * - Dashboard
+ * - Products Management
+ * - Categories Management
+ * - Orders Management
+ * - Customers Management
+ * - Settings
+ * etc.
+ */
+
+// export { default as Dashboard } from './Dashboard';
---
+++ app/assets/frontoffice/themes/default/Pages/index.tsx
@@ -0,0 +1,14 @@
+/**
+ * Custom Theme Pages Index
+ * 
+ * This file exports all default theme page components used by Inertia:
+ * - Home
+ * - Product List
+ * - Product Details
+ * - Category
+ * - Cart
+ * - Checkout
+ * etc.
+ */
+
+// export { default as Home } from './Home';
