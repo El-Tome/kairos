e2ac1e64d5e0eb634cda3fdabe47990f3b67da1e
grintzdel <mathis.oudin@etu.uca.fr>
2025-02-02 14:49:09 +0100

    :wrench: Added webpack and typescript config

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -1,73 +1,127 @@
 const Encore = require('@symfony/webpack-encore');
+const path = require('path');
 
 // Manually configure the runtime environment if not already configured yet by the "encore" command.
-// It's useful when you use tools that rely on webpack.config.js file.
 if (!Encore.isRuntimeEnvironmentConfigured()) {
     Encore.configureRuntimeEnvironment(process.env.NODE_ENV || 'dev');
 }
 
 Encore
-    // directory where compiled assets will be stored
-    .setOutputPath('public/build/')
-    // public path used by the web server to access the output path
-    .setPublicPath('/build')
-    // only needed for CDN's or subdirectory deploy
-    //.setManifestKeyPrefix('build/')
 
-    /*
-     * ENTRY CONFIG
-     *
-     * Each entry will result in one JavaScript file (e.g. app.js)
-     * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
-     */
+    .setOutputPath('public/build/')
+
+    .setPublicPath('/build')
+
     .addEntry('app', './assets/app.js')
 
-    // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
     .splitEntryChunks()
 
-    // will require an extra script tag for runtime.js
-    // but, you probably want this, unless you're building a single-page app
     .enableSingleRuntimeChunk()
 
-    /*
-     * FEATURE CONFIG
-     *
-     * Enable & configure other features below. For a full
-     * list of features, see:
-     * https://symfony.com/doc/current/frontend.html#adding-more-features
-     */
     .cleanupOutputBeforeBuild()
     .enableBuildNotifications()
     .enableSourceMaps(!Encore.isProduction())
-    // enables hashed filenames (e.g. app.abc123.css)
+
     .enableVersioning(Encore.isProduction())
 
-    // configure Babel
-    // .configureBabel((config) => {
-    //     config.plugins.push('@babel/a-babel-plugin');
-    // })
+    .configureBabel((config) => {
+        config.plugins.push('@babel/plugin-syntax-dynamic-import');
+    })
 
-    // enables and configure @babel/preset-env polyfills
     .configureBabelPresetEnv((config) => {
         config.useBuiltIns = 'usage';
         config.corejs = '3.38';
     })
 
-    // enables Sass/SCSS support
-    //.enableSassLoader()
+    .enableTypeScriptLoader(options => {
+        options.transpileOnly = true;
+        options.configFile = path.resolve(__dirname, 'tsconfig.json');
+    })
 
-    // uncomment if you use TypeScript
-    .enableTypeScriptLoader()
-
-    // uncomment if you use React
     .enableReactPreset()
 
-    // uncomment to get integrity="..." attributes on your script & link tags
-    // requires WebpackEncoreBundle 1.4 or higher
-    //.enableIntegrityHashes(Encore.isProduction())
+    .addAliases({
+        '@': path.resolve(__dirname, 'assets'),
+        '@backoffice': path.resolve(__dirname, 'assets/backoffice'),
+        '@frontoffice': path.resolve(__dirname, 'assets/frontoffice'),
+        '@shared': path.resolve(__dirname, 'assets/shared'),
+    })
 
-    // uncomment if you're having problems with a jQuery plugin
-    //.autoProvidejQuery()
+    .enableTypeScriptLoader(options => {
+        options.transpileOnly = true;
+        options.configFile = path.resolve(__dirname, 'tsconfig.json');
+    })
+
+    .enableReactPreset()
+
+    .addRule({
+        test: /\.(ts|tsx|js|jsx)$/,
+        include: path.resolve(__dirname, 'assets'),
+        exclude: /node_modules/,
+        resolve: {
+            extensions: ['.tsx', '.ts', '.js', '.jsx', '.json', '.css']
+        }
+    })
+
+    .configureDevServerOptions(options => {
+        if (!options.static) {
+            options.static = [];
+        } else if (!Array.isArray(options.static)) {
+            options.static = [options.static];
+        }
+
+        options.liveReload = true;
+        options.hot = true;
+
+        options.historyApiFallback = {
+            rewrites: [
+                {
+                    from: /^\/admin.*$/,
+                    to: '/admin/index.html'
+                },
+                {
+                    from: /./,
+                    to: '/index.html'
+                }
+            ]
+        };
+
+        options.headers = {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+            'Access-Control-Allow-Headers': 'X-Requested-With, content-type, Authorization'
+        };
+    })
+
+    .enablePostCssLoader()
+
+    .copyFiles({
+        from: './assets/images',
+        to: 'images/[path][name].[hash:8].[ext]',
+        pattern: /\.(png|jpg|jpeg|gif|ico|svg|webp)$/
+    })
+    .copyFiles({
+        from: './assets/fonts',
+        to: 'fonts/[path][name].[hash:8].[ext]',
+        pattern: /\.(woff|woff2|eot|ttf|otf)$/
+    })
+
+    .configureSplitChunks(splitChunks => {
+        splitChunks.cacheGroups = {
+            vendors: {
+                test: /[\\/]node_modules[\\/]/,
+                priority: -10,
+                name: 'vendors',
+                chunks: 'all'
+            },
+            common: {
+                minChunks: 2,
+                priority: -20,
+                chunks: 'all',
+                name: 'common'
+            }
+        };
+    })
 ;
 
 module.exports = Encore.getWebpackConfig();
