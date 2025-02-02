d49b846350bf98057ef4203da74891b807bc4af0
grintzdel <mathis.oudin@etu.uca.fr>
2025-02-02 14:53:27 +0100

    :necktie: Added Standalone import components files

---
+++ app/assets/backoffice/standalone/index.ts
@@ -0,0 +1,13 @@
+/**
+ * Backoffice Standalone Components Index
+ * 
+ * This file imports all React components that need to be mounted 
+ * in admin Twig templates:
+ * - Admin Navigation
+ * - Quick Actions
+ * - Notifications Panel
+ * - User Menu
+ * etc.
+ */
+
+// import './AdminNavigation';
---
+++ app/assets/frontoffice/standalone/index.ts
@@ -0,0 +1,14 @@
+/**
+ * Frontoffice Standalone Components Index
+ * 
+ * This file imports all React components that need to be mounted 
+ * in frontoffice Twig templates:
+ * - Header
+ * - Shopping Cart
+ * - Newsletter Form
+ * - Cookie Banner
+ * - Search Bar
+ * etc.
+ */
+
+// import './Header';
